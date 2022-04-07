c5030909158e8080e99738ed3f0970ce0e02ff95
tildemit <chandra.am@northeastern.edu>
2022-04-07 14:58:28 -0400

    Fixes to caller.js (a for-loop was removed at some point)

--- caller.js
+++ caller.js
@@ -8,27 +8,28 @@ var clues = [];
 /** Function to request data from jService */
 function initRequests() {
   /** Create a random offset value using Math.random() so clue categories are randomized */
-  var x =  Math.floor(1 + Math.random() * 18408);
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://jservice.io/api/categories?count=6', false)
-  request.open('GET', 'https://jservice.io/api/categories?count=6&offset=' + x ,false)
-  request.send();
-  /** Parse response from API */
-  var arr = JSON.parse(request.response);
+  var arr = []
+  for (var i = 0; i < 6; i++) {
+    var x =  Math.floor(1 + Math.random() * 18408);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://jservice.io/api/categories?count=6&offset=' + x ,false)
+    request.send();
+    /** Parse response from API */
+    arr.push(JSON.parse(request.response)[0]);
+    request.abort();
+  }
 
-  request.abort();
   arr.forEach(cat => {
     /** Push title name into categories */
     categories.push(cat.title);
     var clue_req = new XMLHttpRequest();
     clue_req.open("GET", "https://jservice.io/api/clues?category="+cat.id, false);
     clue_req.send();
-    var arr2 = JSON.parse(clue_req.response);
+    var cluarr = JSON.parse(clue_req.response);
     /** Push five clues into clues */
     for(var i = 0; i < 5; i++){
-      clues.push(arr2[i]); 
+      clues.push(cluarr[i]); 
     }
-    
     clue_req.abort();
   });
 }
@@ -38,8 +39,9 @@ function display() {
     for (var j = 0; j < 6; j++) { 
         if (i == 0) {
            grid[i][j].button_text = categories[j];
-        } else {
-            grid[i][j].button_text = "" + (200 * i);
+        } 
+        else {
+           grid[i][j].button_text = "" + (200 * i);
            var my_clue = clues[5 * j + (i-1)];
            grid[i][j].clue = my_clue.question;
            grid[i][j].value = my_clue.value;
